e2b3c4bf6f48b77426771d552e671aaf5dc45765
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-03-15 14:27:54 +0200

    Implement rough top bar with navigation functionality

--- app/src/main/java/com/example/mybudgetapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/mybudgetapp/ui/screens/HomeScreen.kt
@@ -1,29 +1,27 @@
 package com.example.mybudgetapp.ui.screens
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.mybudgetapp.ui.topbars.MainTopBar
 
 @Composable
 fun HomeScreen(navController: NavController){
+    Scaffold (
+        topBar = { MainTopBar(navController) }
+    ){ innerPadding ->
         Column (
             modifier = Modifier
                 .fillMaxWidth()
+                .padding(innerPadding)
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
 
@@ -33,4 +31,5 @@ fun HomeScreen(navController: NavController){
             )
 
         }
+    }
 }
