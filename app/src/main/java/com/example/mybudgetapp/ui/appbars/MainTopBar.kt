e04af81f5977bfaf87287eea01ad5afe4883b425
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-03-16 19:39:27 +0200

    Arrange month and year dropdown as a scrollable row

--- app/src/main/java/com/example/mybudgetapp/ui/appbars/MainTopBar.kt
+++ app/src/main/java/com/example/mybudgetapp/ui/appbars/MainTopBar.kt
@@ -1,11 +1,21 @@
 package com.example.mybudgetapp.ui.appbars
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.DropdownMenu
@@ -24,10 +34,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -54,30 +66,27 @@ fun MainTopBar(navController: NavController, onMonthYearSelected:(String,String)
         ),
         title = {
             Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
             ){
                 Text(
                     text = "My Budget App",
                     style = MaterialTheme.typography.titleLarge,
                     fontWeight = FontWeight.Bold,
                     fontFamily = FontFamily.Serif,
-                    textAlign = TextAlign.Center,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 10.dp)
                 )
                 //Month and Year text with the dropdown icon in a Row
                 Row (
                     verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
                     modifier = Modifier
                         .fillMaxWidth()
 
                 ){
                     Text(
                         text = selectedMonthYear,
-                        style = MaterialTheme.typography.bodySmall,
+                        fontSize = 20.sp,
                         fontFamily = FontFamily.Serif,
                         color = MaterialTheme.colorScheme.onSecondary
                     )
@@ -92,22 +101,57 @@ fun MainTopBar(navController: NavController, onMonthYearSelected:(String,String)
                     // Dropdown menu for month/year
                     DropdownMenu(
                         expanded = expanded,
-                        onDismissRequest = {expanded = false}
+                        onDismissRequest = {expanded = false},
+                        modifier = Modifier
+                            .background(Color.White)
                     ) {
-                        monthYearList.forEach { monthYear ->
-                            DropdownMenuItem(
-                                text = { Text(monthYear) },
+                        Row (
+                            modifier = Modifier
+                                .horizontalScroll(rememberScrollState())
+                                .padding(2.dp),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ){
+                            monthYearList.forEach { monthYear ->
+                                val dividedDate = monthYear.split(" ")
+                                val month = dividedDate[0]
+                                val year = dividedDate[1]
+                                val isSelected = monthYear == selectedMonthYear
 
-                                onClick = {
-                                    selectedMonthYear = monthYear
-                                    val dividedDate = monthYear.split(" ")
-                                    val month = dividedDate[0]
-                                    val year = dividedDate[1]
-                                    onMonthYearSelected(month, year)
-                                    expanded = false
+                                Box(
+                                    contentAlignment = Alignment.Center,
+                                    modifier = Modifier
+                                        .clickable{
+                                            selectedMonthYear = monthYear
+                                            onMonthYearSelected(month, year)
+                                            expanded = false
+                                        }
+                                        .background(
+                                            color = if (isSelected) Color.Gray else Color.White,
+                                            shape = RoundedCornerShape(16.dp)
+                                        )
+                                        .border(2.dp, Color.Blue, shape = RoundedCornerShape(16.dp))
+                                        .padding(horizontal = 16.dp, vertical = 8.dp)
+
+                                ){
+                                    Column (
+                                        horizontalAlignment = Alignment.CenterHorizontally
+                                    ){
+                                        Text(
+                                            text = month.substring(0,3),
+                                            color = if (isSelected) Color.White else Color.Black,
+                                            fontSize = 20.sp
+
+                                        )
+                                        Text(
+                                            text = year,
+                                            color = if (isSelected) Color.White else Color.Black,
+                                            fontSize = 18.sp
+                                        )
+                                    }
                                 }
-                            )
+                            }
                         }
+
                     }
                 }
             }
@@ -125,5 +169,5 @@ fun generateMonthYearList(currentDate: LocalDate, noOfMonthsBack: Int): List<Str
         monthYearList.add(date.format(formatter))
         date = date.minusMonths(1)
     }
-    return monthYearList.reversed()
+    return monthYearList
 }
