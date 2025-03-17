16376a4f0d0791bd76f82be4d2d462e7c74d918e
Archana Ojha <t3ojar01@students.oamk.fi>
2025-03-17 07:49:48 +0200

    added button for adding income, expenses, to show the transactions and profile

--- app/src/main/java/com/example/mybudgetapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/mybudgetapp/ui/screens/HomeScreen.kt
@@ -1,12 +1,12 @@
 package com.example.mybudgetapp.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,39 +14,117 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ReceiptLong
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material.icons.filled.ReceiptLong
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.ButtonDefaults
 import androidx.navigation.NavController
 import com.example.mybudgetapp.ui.appbars.BottomBar
 import com.example.mybudgetapp.ui.appbars.MainTopBar
 
 @Composable
-fun HomeScreen(navController: NavController){
+fun HomeScreen(navController: NavController) {
 
     // Declare mutable state for selected month and year
     var selectedMonth by remember { mutableStateOf("") }
     var selectedYear by remember { mutableStateOf("") }
 
     // Function to update the selected month and year
-    fun onMonthYearSelected(month:String, year:String){
+    fun onMonthYearSelected(month: String, year: String) {
         selectedMonth = month
         selectedYear = year
     }
 
-    Scaffold (
+    Scaffold(
         topBar = { MainTopBar(navController, ::onMonthYearSelected) },
         bottomBar = { BottomBar(navController) }
-    ){ innerPadding ->
-        Column (
+    ) { innerPadding ->
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(innerPadding)
+                .padding(16.dp)
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
-
         ) {
-            Text(
-                text = "This is home screen"
-            )
+            Text(text = "This is home screen")
+            Spacer(modifier = Modifier.height(16.dp))
 
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                //btn for add income
+                Button(
+                    onClick = { },
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(80.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
+                ) {
+                    Icon(imageVector = Icons.Default.AttachMoney, contentDescription = "Add Income")
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(text = "Add Income")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                //btn for add expense
+                Button(
+                    onClick = {  },
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(80.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
+                ) {
+                    Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = "Add Expense")
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(text = "Add Expense")
+                }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            )
+            // btn for transaction page
+            {
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(80.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
+                ) {
+                    Icon(imageVector = Icons.AutoMirrored.Filled.ReceiptLong, contentDescription = "Transactions")
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(text = "Transactions")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                //btn for profile
+
+                Button(
+                    onClick = { },
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(80.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
+                ) {
+                    Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Profile")
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(text = "Profile")
+                }
+            }
         }
     }
 }
