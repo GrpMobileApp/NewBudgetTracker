02d3cc6e1fb3a95a833be1d36c6d041f1d2a5505
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-03-31 15:42:22 +0300

    Sub category will appear according to selected main category

--- app/src/main/java/com/example/mybudgetapp/ui/components/AddExpenseDoalog.kt
+++ app/src/main/java/com/example/mybudgetapp/ui/components/AddExpenseDoalog.kt
@@ -1,5 +1,6 @@
 package com.example.mybudgetapp.ui.components
-import android.app.Dialog
+
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,13 +8,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,16 +24,39 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mybudgetapp.ui.viewModel.MainCategoryViewModel
+import com.example.mybudgetapp.ui.model.SubCategoryItem
+import com.example.mybudgetapp.ui.viewModel.SharedViewModel
+
 
 @Composable
-fun AddExpenseDialog(onDismiss: () -> Unit, onSubmit: (String, String, String, Float) -> Unit, mainCategoryViewModel: MainCategoryViewModel){
+fun AddExpenseDialog(
+    onDismiss: () -> Unit,
+    onSubmit: (String, String, String, Float) -> Unit,
+    mainCategoryViewModel: MainCategoryViewModel
+){
     var category by remember { mutableStateOf("") }
     var subCategory by remember { mutableStateOf("") }
     var note by remember { mutableStateOf("") }
     var amount by remember { mutableStateOf("") }
+    val sharedViewModel: SharedViewModel = viewModel()
+
+    // State to hold the budgetId
+    val budgetId by sharedViewModel.budgetId.collectAsState()
+    val userId = "v3Udk1WkxbV4YqoyNgLL"
 
     val mainCategoryList by mainCategoryViewModel.mainCategoryList.collectAsState()
+    val mainCategoryWithSubcategories by mainCategoryViewModel.mainCategoryWithSubcategories.collectAsState()
+
+    // Filtered subcategories based on selected main category
+    var subCategoriesOfSelectedMain by remember { mutableStateOf<List<SubCategoryItem>>(emptyList()) }
+
+    // Fetch main categories & subcategories when dialog opens
+    LaunchedEffect(Unit) {
+        mainCategoryViewModel.fetchMainCategoryWithSubcategories(userId,budgetId!!)
+    }
+
     Dialog (onDismissRequest = { onDismiss() }) {
         Surface (
             modifier = Modifier
@@ -54,16 +78,25 @@ fun AddExpenseDialog(onDismiss: () -> Unit, onSubmit: (String, String, String, F
                     label = "Select Category"
                 ) { selectedCategory ->
                     category = selectedCategory
+                    Log.d("AddExpenseDialog", "selectedCategory '$selectedCategory' saved.")
+
+                    // Update subcategories based on the selected category
+                    val selectedMainCategory = mainCategoryWithSubcategories.find { it.mainCategoryName == category }
+                    subCategoriesOfSelectedMain = selectedMainCategory?.subCategories ?: emptyList()
+                    Log.d("AddExpenseDialog", "Subcategories for '$category': ${subCategoriesOfSelectedMain.map { it.subCategoryName }}")
                 }
 
                 Spacer(modifier = Modifier.height(10.dp))
 
-                TextField(
-                    value = subCategory,
-                    onValueChange = { subCategory = it },
-                    label = { Text("Enter Subcategory") },
-                    modifier = Modifier.fillMaxWidth()
-                )
+                // Dropdown for Subcategory
+                DropdownLayout(
+                    listOfItems = subCategoriesOfSelectedMain.map { it.subCategoryName },
+                    label = "Select Subcategory"
+                ) { selectedSubCategory ->
+                    subCategory = selectedSubCategory
+                    Log.d("AddExpenseDialog", "Subcategory selected: '$subCategory'")
+                }
+
                 Spacer(modifier = Modifier.height(10.dp))
 
                 TextField(
